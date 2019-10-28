bf4242c20b470db2449e2ff2b0d6443e7e944e0e
Nydragon00 <56591727+Nydragon00@users.noreply.github.com>
2019-10-28 22:03:19 +0100

    a bit of cleaning

--- Terminal_create_file.rb
+++ Terminal_create_file.rb
@@ -4,19 +4,19 @@ class Handle_file
       if key_word == "create" && !File.file?(file)
         File.new(file, File::CREAT)
       elsif File.file?(file) == true
-        puts "file exists"
+        puts "\nFile exists."
       end
     rescue
-      puts "An error has occured"
+      puts "An error has occured."
     end
   end
   def delete_file(key_word, file)
-    f = File.open(file)
     if key_word == "delete" && File.file?(file)
       begin
+        f = File.open(file)
         File.delete(f)
       rescue
-        puts "An error has occured"
+        puts "An error has occured."
       end
     end
   end
@@ -26,7 +26,7 @@ class Handle_file
       begin
         f.close
       rescue
-        puts "An error has occured"
+        puts "An error has occured."
       end
     end
   end
@@ -47,7 +47,7 @@ class Handle_file
         data = data.tr("_", " ")
         File.write(file, data + "\n ", mode: 'a')
       rescue
-        puts "An error has occured"
+        puts "An error has occured."
       end
     end
     if key_word == "append" && mode == "space"
@@ -55,7 +55,7 @@ class Handle_file
         data = data.tr("_", " ")
         File.write(file, data + " ", mode: 'a')
       rescue
-        puts "An error has occured"
+        puts "An error has occured."
       end
     end
   end
@@ -63,65 +63,77 @@ end
 
 class Help
   def man()
-
-    puts "\nWich page do you want to open? " #add the different topics
     while true
+      print "\nWich page do you want to open: " #add the different topics
       option = gets.chomp
       case option
       when "1"
-        puts "\nWe got help to 5 elements: \ncreate \ndelete \nclose \nread \nappend\n\n"
+        puts "\nWe got help for 5 elements: \n-create \n-delete \n-close \n-read \n-append\n"
         while true
+          print "\nEnter your choice: "
           choice = gets.chomp
           if ["create", "delete", "close", "read", "append"].include?choice
             if choice == "create"
-              puts "create is:"
+              puts "\ncreate is:"
+            end
+            if choice == "delete"
+              puts "\ndelete is:"
+            end
+            if choice == "close"
+              puts "\nclose is:"
+            end
+            if choice == "read"
+              puts "\nread is:"
+            end
+            if choice == "append"
+              puts "\nappend is:"
             end
           end
           if choice == "end"
-            puts "End has been registrated"
+            puts "\nEnd has been registrated."
             break
           end
         end
       end
       if option == "end"
-        puts "End has been registrated"
+        puts "\nEnd has been registrated."
         break
       end
     end
   end
 end
 
-command = "empty"
-
-while true
-  print "Enter the command: "
-  command = gets.chomp
-  command = command.split(' ')
-  if command[0] == "end"
-    break
-  end
-  if command[0] == "create"
-    entry = Handle_file.new
-    entry.create_file(command[0], command[1])
-  end
-  if command[0] == "delete"
-    entry = Handle_file.new
-    entry.delete_file(command[0], command[1])
-  end
-  if command[0] == "close"
-    entry = Handle_file.new
-    entry.close_file(command[0], command[1])
-  end
-  if command[0] == "read"
-    entry = Handle_file.new
-    entry.read_file(command[0], command[1])
-  end
-  if command[0] == "append"
-    entry = Handle_file.new
-    entry.append_file(command[0], command[1], command[2], command[3])
-  end
-  if command[0] == "man"
-    entry = Help.new
-    entry.man()
+def core
+  command = "empty"
+  while true
+    print "\nEnter the command: "
+    command = gets.chomp
+    command = command.split(' ')
+    case command[0]
+    when "end"
+      puts "\nEnd has been registrated."
+      break
+    when "create"
+      entry = Handle_file.new
+      entry.create_file(command[0], command[1])
+    when "delete"
+      entry = Handle_file.new
+      entry.delete_file(command[0], command[1])
+    when "close"
+      entry = Handle_file.new
+      entry.close_file(command[0], command[1])
+    when "read"
+      entry = Handle_file.new
+      entry.read_file(command[0], command[1])
+    when "append"
+      entry = Handle_file.new
+      entry.append_file(command[0], command[1], command[2], command[3])
+    end
+    if command[0] == "man"
+      entry = Help.new
+      entry.man()
+    end
   end
 end
+
+core()
